40054ca25efb7f2c6570c243da5f0c7e30ade88e
keerthi-narayan <keerthinarayan.2002@gmail.com>
2024-08-08 13:17:57 +0530

    initial commit

---
+++ database/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDb= async() => {
+    try{
+        await mongoose.connect(process.env.DB);
+        console.log("database connected");
+
+    } catch(error){
+        console.log(error);
+    }
+}
